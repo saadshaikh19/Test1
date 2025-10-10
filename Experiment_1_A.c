52bb8a28a45e0521ad9434e14ffea8ee7637a9f0
saadshaikh19 <saaaaadshaikh147@gmail.com>
2025-10-10 23:09:46 +0530

    Create Experiment_1_A.c

---
+++ Experiment_1_A.c
@@ -0,0 +1,16 @@
+/*
+Name: Saad Shaikh
+Branch: mechanical F1
+UIN : 251M009*/
+#include <stdio.h>
+int main ()
+{  
+   printf("Name\t:Saad fast\n");
+   printf("Age\t:20\n");
+   printf("Class:F.E mechanical\n");
+   printf("Division:F1\n");
+   printf("UIN\t:251M014\n");
+   
+   return 0;
+   
+}
