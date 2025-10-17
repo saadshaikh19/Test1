22eac25f0fd56f1f8386b0733e98d56d97cea031
Saad <saaaaadshaikh147@gmail.com>
2025-10-17 11:59:53 +0530

    Create EXPERIMENT_2A.c

---
+++ EXPERIMENT_2A.c
@@ -0,0 +1,19 @@
+/*
+Name: Saad shaikh 
+UIN : 251M009
+*/
+#include <stdio.h>
+
+int main (){
+   
+        int x;
+        printf("Enter a number:");
+        scanf("%d",&x);
+        if (x%2==0)
+        printf("%d is even",x);
+else 
+        printf("%d is odd",x);
+        return 0;
+}
+
+
